5d86071660d52e6501fda31babe7ffb738e25170
lctech-jeff <jeff@lctech.com.tw>
2023-07-24 17:19:17 +0800

    fix pinia in build

--- src/main.ce.ts
+++ src/main.ce.ts
@@ -1,11 +1,15 @@
 import { defineCustomElement } from 'vue'
+import { createPinia, setActivePinia } from 'pinia'
+
 import CheckoutBtn from '@/components/CheckoutBtn/Entry.ce.vue'
 import CheckoutPanel from '@/components/CheckoutPanel/Entry.ce.vue'
+
+setActivePinia(createPinia())
 
 export const JCheckoutBtn = defineCustomElement(CheckoutBtn)
 export const JCheckoutPanel = defineCustomElement(CheckoutPanel)
 
-export function register(tagPrefix = 'j') {
+export const register = (tagPrefix = 'j') => {
   customElements.define(`${tagPrefix}-checkout-btn`, JCheckoutBtn)
   customElements.define(`${tagPrefix}-checkout-panel`, JCheckoutPanel)
 }
